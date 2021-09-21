9863ba51522a2fe9d75a4d20ceaf39acb4a70368
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2021-09-21 21:58:43 +0200

    Update user_install.sh

--- user_install.sh
+++ user_install.sh
@@ -9,21 +9,21 @@ rm -f "${HOME}"/.bashrc
 rm -f "${HOME}"/.inputrc
 
 # link bash_profile bashrc and inputrc
-ln -s "${dotfiles}"/bash_profile "${HOME}"/.bash_profile
-ln -s "${dotfiles}"/bashrc "${HOME}"/.bashrc
-ln -s "${dotfiles}"/inputrc "${HOME}"/.inputrc
+ln -s "${dotfiles}"/.bash_profile "${HOME}"/.bash_profile
+ln -s "${dotfiles}"/.bashrc "${HOME}"/.bashrc
+ln -s "${dotfiles}"/.inputrc "${HOME}"/.inputrc
 
 # create "${HOME}"/.config and "${HOME}"/.local if they don't exist
 mkdir -p "${HOME}"/.config
 mkdir -p "${HOME}"/.local/bin
 
 # symlink everything in config
-for file in "${dotfiles}"/config/*; do
+for file in "${dotfiles}"/.config/*; do
 	ln -s "${file}" "${HOME}"/.config/
 done
 
 # symlink everything in local/bin
-for file in "${dotfiles}"/bin/*; do
+for file in "${dotfiles}"/.bin/*; do
 	ln -s "${file}" "${HOME}"/.local/bin/
 done
 
