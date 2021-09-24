ee88b5d171005bbae1d5f6b51fbaa20aaef23f8b
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2021-09-24 20:53:01 +0200

    Update clone_build.sh

--- .local/bin/clone_build.sh
+++ .local/bin/clone_build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+prefix_dir="$HOME/.local"
+
 git clone https://github.com/ifreund/river "$HOME"/river
 
 cd "$HOME"/river
@@ -7,15 +9,15 @@ cd "$HOME"/river
 git submodule update --init
 
 # -Dxwayland for xwayland support
-zig build -Drelease-safe -Dman-pages -Dxwayland --prefix "$HOME"/.local install
+zig build -Drelease-safe -Dman-pages -Dxwayland --prefix $prefix_dir install
 
 git clone https://github.com/Alexays/Waybar "$HOME"/waybar
 
 cd "$HOME"/waybar
 
 # build options enabled/disabled
-meson build -Dlibnl=enabled -Dlibudev=enabled -Dlibevdev=enabled -Dpulseaudio=enabled -Dsystemd=disabled -Ddbusmenu-gtk=disabled -Dman-pages=enabled -Dmpd=disabled -Dgtk-layer-shell=disabled -Drfkill=enabled -Dsndio=disabled -Dtests=disabled
+meson build -Dprefix=$prefix_dir -Dlibnl=enabled -Dlibudev=enabled -Dlibevdev=enabled -Dpulseaudio=enabled -Dsystemd=disabled -Ddbusmenu-gtk=disabled -Dman-pages=enabled -Dmpd=disabled -Dgtk-layer-shell=disabled -Drfkill=enabled -Dsndio=disabled -Dtests=disabled
 
-ninja -C build "$HOME"/.local install
+ninja -C build install
 
 cd "$HOME"
