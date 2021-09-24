0bf6e4897c60d488ed2f30109b280aca871cff21
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2021-09-24 20:35:52 +0200

    Update clone_build.sh

--- .local/bin/clone_build.sh
+++ .local/bin/clone_build.sh
@@ -7,13 +7,13 @@ cd "$HOME"/river
 git submodule update --init
 
 # -Dxwayland for xwayland support
-zig build -Drelease-safe -Dman-pages --prefix "$HOME"/.local install
+zig build -Drelease-safe -Dman-pages -Dxwayland --prefix "$HOME"/.local install
 
 git clone https://github.com/Alexays/Waybar
 
 cd "$HOME"/Waybar
 
 # build options enabled/disabled
-meson build -Dlibnl=enabled -Dlibevdev=disabled -Dlibudev=enabled -Dpulseaudio=enabled -Dsystemd=disabled -Ddbusmenu-gtk=disabled -Dman-pages=enabled -Dmpd=disabled -Dgtk-layer-shell=disabled -Drfkill=enabled -Dsndio=disabled -Dtests=disabled
+meson build -Dlibnl=enabled -Dlibudev=enabled -Dlibevdev=enabled -Dpulseaudio=enabled -Dsystemd=disabled -Ddbusmenu-gtk=disabled -Dman-pages=enabled -Dmpd=disabled -Dgtk-layer-shell=disabled -Drfkill=enabled -Dsndio=disabled -Dtests=disabled
 
 ninja -C build install
