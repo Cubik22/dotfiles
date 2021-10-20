7bc9dd12207346f591d2c2ce2b08daa1d78cc5fd
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2021-10-20 22:10:42 +0200

    load brightness level at start and use shellcheck

--- .local/bin/adjust_brightness.sh
+++ .local/bin/adjust_brightness.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 if [ "$#" -ne 1 ]; then
-    echo "please insert one parameter: 'up' or 'down'"
+    echo "please insert one parameter: 'up' 'down' 'load'"
     echo "you inserted $# parameters"
     exit
 fi
 
 mode=$1
 
-if [ "$mode" != "up" ] && [ "$mode" != "down" ]; then
-   echo "please pass as a parameter 'up' or 'down'"
+if [ "$mode" != "up" ] && [ "$mode" != "down" ] && [ "$mode" != "load" ]; then
+   echo "please pass as a parameter: 'up' 'down' 'load'"
    exit
 fi
 
@@ -23,43 +23,42 @@ Level_Path="$XDG_DATA_HOME/state/brightness_level"
 
 max_brightness="$(cat $Max_Brightness_Path)"
 
-current_level="$(cat $Level_Path)"
+current_level="$(cat "$Level_Path")"
 
 number_steps=10
 
-step="$(( $max_brightness / $number_steps ))"
+step="$(( max_brightness / number_steps ))"
 
-minimum="$(( $max_brightness - ($number_steps - 1) * $step ))"
+min_brightness="$(( max_brightness - (number_steps - 1) * step ))"
 
 if [ "$mode" = "up" ]; then
-	# new_brightness="$(( $current_brightness + $step ))"
+	# new_brightness="$(( current_brightness + step ))"
 	if [ "$current_level" -lt 10 ]; then
-    	new_level="$(( $current_level + 1 ))"
+    	new_level="$(( current_level + 1 ))"
 	else
     	new_level="$current_level"
-	fi
-	new_brightness="$(( $new_level * $step ))"
-
-	if [ $new_brightness -lt "$(( $max_brightness + 1 ))" ]; then
-		echo $new_brightness > $Brightness_Path
-	else
-		echo $max_brightness > $Brightness_Path
 	fi
 elif [ "$mode" = "down" ]; then
-	# new_brightness="$(( $current_brightness - $step ))"
+	# new_brightness="$(( current_brightness - step ))"
 	if [ "$current_level" -gt 1 ]; then
-    	new_level="$(( $current_level - 1 ))"
+    	new_level="$(( current_level - 1 ))"
 	else
     	new_level="$current_level"
 	fi
-	new_brightness="$(( $new_level * $step ))"
+elif [ "$mode" = "load" ]; then
+	# new_brightness="$current_brightness"
+    new_level="$current_level"
+fi
 
-	if [ $new_brightness -gt $minimum ]; then
-		echo $new_brightness > $Brightness_Path
-	else
-		echo $minimum > $Brightness_Path
-	fi
+new_brightness="$(( new_level * step ))"
+
+if [ "$new_brightness" -ge "$(( max_brightness + 1 ))" ]; then
+	echo "$max_brightness" > "$Brightness_Path"
+elif [ "$new_brightness" -le "$min_brightness" ]; then
+	echo "$min_brightness" > "$Brightness_Path"
+else
+	echo "$new_brightness" > "$Brightness_Path"
 fi
 
 echo "$new_level" > "$Level_Path"
-echo "$(( $new_level * 10 ))"
+echo "$(( new_level * 10 ))"
