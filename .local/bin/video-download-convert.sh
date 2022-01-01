6f91d7fe2b0a112b088d8a816858a987169fa7e1
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2022-01-01 23:33:11 +0100

    video download

---
+++ .local/bin/video-download-convert.sh
@@ -0,0 +1,31 @@
+#/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "error: please insert just one parameter"
+    exit
+fi
+
+url="$1"
+
+mkdir tmp
+cd tmp
+
+youtube-dl \
+--ignore-errors \
+--retries=infinite \
+--add-metadata \
+--xattrs \
+"$url"
+
+name="$(echo *)"
+name_ext="$(echo "$name" | rev | cut -d "." -f 1 | rev)"
+name_no_ext="$(echo "$name" | rev | cut -d "." -f 2- | rev)"
+name_ogg="${name_no_ext}.ogg"
+
+if [ ! "$name_ext" = "ogg" ]; then
+    ffmpeg -loglevel quiet -i "$name" "$name_ogg"
+fi
+
+mv "$name_ogg" ../
+cd ..
+rm -r tmp
