a9a1dab456ecec0276c92d3cc23b58fea6a1d8ac
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2022-02-03 05:43:41 +0100

    fuzzel

--- .local/bin/fuzzel.sh
+++ .local/bin/fuzzel.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-fuzzel  -f "JetBrainsMono Nerd Font Mono:style=Medium:size=14" \
-        -D no -P "" -T footclient -l 20 \
-        -w 64 -x 32 -y 8 -p 0 \
-        -b ebdbb2ff -t 1d2021ff -m 458588ff -s d5c4a1ff -S 3c3836ff \
-        -B 2 -r 0 -C 6e6e6eff $@ <&0
+# white
+        # --background=ebdbb2ff --text-color=1d2021ff \
+        # --match-color=458588ff \
+        # --selection-color=d5c4a1ff --selection-text-color=3c3836ff \
+
+# black
+fuzzel  --font="JetBrainsMono Nerd Font Mono:style=Medium:size=14" \
+        --dpi-aware=no --prompt="" --terminal=footclient --lines=20 \
+        --width=64 --horizontal-pad=32 --vertical-pad=8 --inner-pad=0 \
+        --background=1d2021ff --text-color=d5c4a1ff \
+        --match-color=cc241dff \
+        --selection-color=3c3836ff --selection-text-color=fbf1c7ff \
+        --border-width=2 --border-radius=0 --border-color=6e6e6eff $@ <&0
