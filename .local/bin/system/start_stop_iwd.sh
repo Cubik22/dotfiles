9dbd6f1fef5095cc0866bc47df9069e1d92ce90e
lbia.xyz <mail@lbia.xyz>
2022-06-20 16:17:03 +0200

    sv-smart
    new script to call sv commands checking status first
    fix type missing / in /dev/null

--- .local/bin/system/start_stop_iwd.sh
+++ .local/bin/system/start_stop_iwd.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-status=$(doas sv status iwd | awk -F ":" '{print $1}')
+status="$(doas sv status iwd | awk -F ":" '{print $1}')"
 
 if [ "$status" = "run" ]; then
     doas sv down iwd
 elif [ "$status" = "down" ]; then
     doas sv up iwd
 else
-    echo "ERROR: iwd status is $status"
+    echo "error: iwd status is $status"
 fi
